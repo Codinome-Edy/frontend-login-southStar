00778607c3e9dab0728a5625ab5a26ef721a6baa
Eduardo Silva <eduardovieira201318@gmail.com>
2024-08-16 18:05:27 -0300

    Início ddo corpo da página de Login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,8 +1,36 @@
+import {FaUser, FaLock} from "react-icons/fa";
+
 export default function Login(){
     return (
         <>
-            <h1>Hello World</h1>
-            <a href="/">Home</a>      
+        <a href="/">Home</a>
+            <div className="login.container">
+                <form>
+                    <h1>Entre no SouthStar</h1>
+                    <div>
+                        <input type="email" placeholder="E-mail" />
+                        <FaUser className="icon" />
+                    </div>
+                    <div>
+                        <input type="passowrd" placeholder="Senha" />
+                        <FaLock className="icon" />
+                    </div>
+
+                    <div className="recall-forget">
+                        <label>
+                            <input type="checkbox" />
+                            Lembre de mim
+                        </label>
+                        <a href="#">Esqueceu a senha?</a>
+                    </div>
+
+                    <button>Entrar</button>
+
+                    <div className="signup-link">
+                        <p>Não tem uma conta?</p>
+                    </div>
+                </form>
+            </div>
         </>
     )
 }
